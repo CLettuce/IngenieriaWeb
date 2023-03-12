a4b308b36cd766c3415a22993cff16ab191e7504
robert129ur@gmail.com <robert129ur@gmail.com>
2023-03-11 23:50:20 -0600

    waterver

--- IngenieriaWeb/Models/Cmarca.cs
+++ IngenieriaWeb/Models/Cmarca.cs
@@ -5,7 +5,33 @@ using System.Web;
 
 namespace IngenieriaWeb.Models
 {
-    public class Cmarca
+    public abstract class Cmarca
     {
+        public class Disti
+        {
+            public int Id { get; set; }
+            public string N_Marca { get; set; }
+            public string Modelo { get; }
+
+
+            public Disti(int Id, string N_Marca, string Modelo)
+            {
+                this.Id = Id;
+                this.N_Marca = N_Marca;
+                this.Modelo = Modelo;
+
+            }
+            public static List<Disti> Marca12()
+            {
+                return new List<Disti>
+                {
+                    new Disti(1,"Intel","I3"),
+                    new Disti(2,"AMD","HQ"),
+                    new Disti(3,"CR","JK")
+                };
+            
+            }
+        }
     }
+    
 }
